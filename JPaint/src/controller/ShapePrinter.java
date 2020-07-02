117a7613b294ca1c64a4127d76ec6f1e56e0b57f
Steven <Steven@DESKTOP-JGL5F9H.attlocal.net>
2020-07-02 01:06:53 -0500

    Triangles now print

--- JPaint/src/controller/ShapePrinter.java
+++ JPaint/src/controller/ShapePrinter.java
@@ -22,11 +22,13 @@ public class ShapePrinter implements IPrinter{
 	public void print() {
 		ShapeList<IShape> shapeList = paintCanvas.getShapes();
 		for (IShape shape : shapeList) {
+			//Get each shapes attributes
 			ShapeType sType = shape.getType();
 			ShapeColor sColor = shape.getColor();
 			Point sOrigin = shape.getOrigin();
 			int sWidth = shape.getWidth();
 			int sHeight = shape.getHeight();
+			//Transfer from enum to Color class
 			Color currC;
 			try {
 				Field field = Class.forName("java.awt.Color").getField(sColor.toString());
@@ -34,15 +36,27 @@ public class ShapePrinter implements IPrinter{
 			} catch (Exception e2) {
 				currC = null; // Not defined
 			}
+			//Print the object based on its ShapeType
 			Graphics2D graphics2d = paintCanvas.getGraphics2D();
 			graphics2d.setColor(currC);
-			//Actual case example for when we have more shape types
-//			if (sType.equals(ShapeType.RECTANGLE)) {
 			if (sType.equals(ShapeType.ELLIPSE)) {
 				graphics2d.fillOval(sOrigin.getX(), sOrigin.getY(), sWidth, sHeight);
 			}
 			else if(sType.equals(ShapeType.RECTANGLE)) {
 				graphics2d.fillRect(sOrigin.getX(), sOrigin.getY(), sWidth, sHeight);
+			}
+			else if(sType.equals(ShapeType.TRIANGLE)) {
+				int[] xDimensions = new int[3];
+				int[] yDimensions = new int[3];
+				//Get x dimensions for triangle
+				xDimensions[0] = sOrigin.getX();
+				xDimensions[1] = sOrigin.getX() + (sWidth / 2);
+				xDimensions[2] = sOrigin.getX() + sWidth;
+				//Get y dimensions for triangle
+				yDimensions[0] = sOrigin.getY() + sHeight;
+				yDimensions[1] = sOrigin.getY();
+				yDimensions[2] = sOrigin.getY() + sHeight;
+				graphics2d.fillPolygon(xDimensions, yDimensions, 3);
 			}
 		}
 	}
